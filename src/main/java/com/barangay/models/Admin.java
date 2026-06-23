6ab238a3017e7695c59e99e3235ba3a3bba48e02
LynielRanches <lyniel.ranches.college@ustangelicum.edu.ph>
2026-06-23 12:30:48 +0800

    added admin.java code

--- src/main/java/com/barangay/models/Admin.java
+++ src/main/java/com/barangay/models/Admin.java
@@ -1,5 +1,25 @@
-package main.java.com.barangay.models;
+package com.barangay.models;
 
-public class Admin {
-    
+public class Admin extends Staff {
+
+    public Admin(String employeeId,
+                 String fullName) {
+
+        super(employeeId, fullName);
+    }
+
+    @Override
+    public void performDuty() {
+
+        System.out.println(
+                "Admin is managing the system."
+        );
+    }
+
+    @Override
+    public String toString() {
+
+        return "Admin -> " +
+                super.toString();
+    }
 }
