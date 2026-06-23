5be056ae9e4a0fd029771ab1b3f440e08028ab6d
LynielRanches <lyniel.ranches.college@ustangelicum.edu.ph>
2026-06-23 23:26:49 +0800

    added resident repository code

--- src/main/java/com/barangay/Repositories/ResidentRepository.java
+++ src/main/java/com/barangay/Repositories/ResidentRepository.java
@@ -1,5 +1,46 @@
-package main.java.com.barangay.Repositories;
+package com.barangay.repositories;
 
-public class ResidentRepository {
-    
+import com.barangay.interfaces.Searchable;
+import com.barangay.models.Resident;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResidentRepository
+        implements GenericRepository<Resident>,
+                   Searchable<Resident> {
+
+    private final List<Resident> residents;
+
+    public ResidentRepository() {
+        residents = new ArrayList<>();
+    }
+
+    @Override
+    public void add(Resident resident) {
+        residents.add(resident);
+    }
+
+    @Override
+    public void remove(Resident resident) {
+        residents.remove(resident);
+    }
+
+    @Override
+    public List<Resident> getAll() {
+        return new ArrayList<>(residents);
+    }
+
+    @Override
+    public Resident searchById(String id) {
+
+        for (Resident resident : residents) {
+
+            if (resident.getResidentId().equalsIgnoreCase(id)) {
+                return resident;
+            }
+        }
+
+        return null;
+    }
 }
