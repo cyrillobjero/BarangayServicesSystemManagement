f9ec537b093013ad57fcbb5c434176af14d3b040
LynielRanches <lyniel.ranches.college@ustangelicum.edu.ph>
2026-06-24 21:51:54 +0800

    added code for transacrepo

--- src/main/java/com/barangay/Repositories/TransactionRepository.java
+++ src/main/java/com/barangay/Repositories/TransactionRepository.java
@@ -1,5 +1,48 @@
-package main.java.com.barangay.Repositories;
+package com.barangay.repositories;
 
-public class TransactionRepository {
-    
+import com.barangay.interfaces.Searchable;
+import com.barangay.models.TransactionLog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionRepository
+        implements GenericRepository<TransactionLog>,
+                   Searchable<TransactionLog> {
+
+    private final List<TransactionLog> transactions;
+
+    public TransactionRepository() {
+        transactions = new ArrayList<>();
+    }
+
+    @Override
+    public void add(TransactionLog transaction) {
+        transactions.add(transaction);
+    }
+
+    @Override
+    public void remove(TransactionLog transaction) {
+        transactions.remove(transaction);
+    }
+
+    @Override
+    public List<TransactionLog> getAll() {
+        return new ArrayList<>(transactions);
+    }
+
+    @Override
+    public TransactionLog searchById(String id) {
+
+        for (TransactionLog transaction : transactions) {
+
+            if (transaction.getTransactionId()
+                    .equalsIgnoreCase(id)) {
+
+                return transaction;
+            }
+        }
+
+        return null;
+    }
 }
