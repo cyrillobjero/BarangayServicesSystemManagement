3f14cbb216ef376a19490de723205b67e9698859
cyrillobjero <cyrillmay.objero.college@ustangelicum.edu.ph>
2026-06-26 17:12:47 +0800

    added clerk

--- src/main/java/com/barangay/models/Clerk.java
+++ src/main/java/com/barangay/models/Clerk.java
@@ -1,5 +1,13 @@
-package main.java.com.barangay.models;
+package com.barangay.models;
 
-public class Clerk {
-    
+public class Clerk extends Staff {
+
+    public Clerk(String staffId, String name) {
+        super(staffId, name);
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("Role: CLERK");
+    }
 }
