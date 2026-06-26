f37dc2935d508c644ad66bc66767728351eb3344
altheavictoria15 <althea.onez.college@ustangelicum.edu.ph>
2026-06-27 00:53:56 +0800

    input validator for util

--- src/main/java/com/barangay/utils/InputValidator.java
+++ src/main/java/com/barangay/utils/InputValidator.java
@@ -1,5 +1,17 @@
-package main.java.com.barangay.utils;
+package com.barangay.utils;
 
 public class InputValidator {
-    
+
+    public static boolean isBlank(
+            String value) {
+
+        return value == null ||
+               value.isBlank();
+    }
+
+    public static boolean isValidAge(
+            int age) {
+
+        return age >= 0;
+    }
 }
