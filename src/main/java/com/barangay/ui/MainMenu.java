09be241408ffe1a600a2627d6541b83da512b549
altheavictoria15 <althea.onez.college@ustangelicum.edu.ph>
2026-06-27 08:06:39 +0800

    main

---
+++ src/main/java/com/barangay/ui/MainMenu.java
@@ -0,0 +1,17 @@
+package com.barangay.ui;
+
+public class MainMenu {
+
+    public void display() {
+
+        System.out.println("""
+                ==========================
+                BARANGAY SYSTEM
+                ==========================
+                1. Residents
+                2. Requests
+                3. Payments
+                4. Reports
+                """);
+    }
+}
