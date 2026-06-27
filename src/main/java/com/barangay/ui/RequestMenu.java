1a041bf107750855b1c98fd7feee30ffb0e81cf9
altheavictoria15 <althea.onez.college@ustangelicum.edu.ph>
2026-06-27 08:12:39 +0800

    request menu

---
+++ src/main/java/com/barangay/ui/RequestMenu.java
@@ -0,0 +1,15 @@
+package com.barangay.ui;
+
+public class RequestMenu {
+
+    public void display() {
+
+        System.out.println("""
+                ==========================
+                REQUEST MENU
+                ==========================
+                1. Submit Request
+                2. Track Request
+                """);
+    }
+}
