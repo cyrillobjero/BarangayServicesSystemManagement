d13282e910f16a8cfa900db589f2625c7de99ac2
altheavictoria15 <althea.onez.college@ustangelicum.edu.ph>
2026-06-27 08:11:40 +0800

    resident menu

--- src/main/java/com/barangay/ui/ResidentMenu.java
+++ src/main/java/com/barangay/ui/ResidentMenu.java
@@ -1,5 +1,15 @@
-package main.java.com.barangay.ui;
+package com.barangay.ui;
 
 public class ResidentMenu {
-    
+
+    public void display() {
+
+        System.out.println("""
+                ==========================
+                RESIDENT MENU
+                ==========================
+                1. Add Resident
+                2. View Residents
+                """);
+    }
 }
