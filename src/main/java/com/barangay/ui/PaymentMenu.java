d9925cb1ab67b502a934db3634269012240f742c
altheavictoria15 <althea.onez.college@ustangelicum.edu.ph>
2026-06-27 08:14:23 +0800

    menu for payment

--- src/main/java/com/barangay/ui/PaymentMenu.java
+++ src/main/java/com/barangay/ui/PaymentMenu.java
@@ -1,5 +1,15 @@
-package main.java.com.barangay.ui;
+package com.barangay.ui;
 
 public class PaymentMenu {
-    
+
+    public void display() {
+
+        System.out.println("""
+                ==========================
+                PAYMENT MENU
+                ==========================
+                1. Cash Payment
+                2. GCash Payment
+                """);
+    }
 }
