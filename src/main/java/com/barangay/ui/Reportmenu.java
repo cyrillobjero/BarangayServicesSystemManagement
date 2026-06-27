e0f6f69d47bf4c3ce98e44c240c7c44f6df69f16
altheavictoria15 <althea.onez.college@ustangelicum.edu.ph>
2026-06-27 08:17:28 +0800

    Update Reportmenu.java

---
+++ src/main/java/com/barangay/ui/Reportmenu.java
@@ -0,0 +1,15 @@
+package com.barangay.ui;
+
+public class ReportMenu {
+
+    public void display() {
+
+        System.out.println("""
+                ==========================
+                REPORT MENU
+                ==========================
+                1. Transaction Report
+                2. Resident Report
+                """);
+    }
+}
